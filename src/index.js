8d7ba2d0f411430efcd6f50aa32ee8dcdf100b09
SergioCostaAndrade <scandrade1968@gmail.com>
2024-02-28 20:21:10 -0300

    Sem banco de dados 9

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ app.get("/", async (req, res) => {
   try {
     //const agenda = await knex('agenda');
     const carros = ["fusca", "cadilac"];
-    return res.status(200).json({ mensagem: "teste com sucesso" });
+    //return res.status(200).json({ mensagem: "teste com sucesso" });
+    return res.status(200).json(carros);
   } catch (error) {
     console.log("erro aqui 2", error);
     return res.status(400).json({ mensagem: "Erro inesperado do servidor" });
